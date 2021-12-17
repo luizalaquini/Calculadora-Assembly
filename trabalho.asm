bd2942683f6d36412ee2af17aa716646e82a17d2
Guilherme Goes Zanetti <guigoes8@hotmail.com>
2021-12-16 22:23:57 -0300

    Finalização
    Co-authored-by: Luiza B. Laquini <luizalaquini@users.noreply.github.com>

--- trabalho.asm
+++ trabalho.asm
@@ -9,23 +9,38 @@ segment code
     mov sp,stacktop
 
     ;Ler os números (dois números de 4 dígitos com sinal)
+    mov dx, mensagemPrimeiroNum
+    mov ah, 9
+    int 21h
     mov bx,num1
     call lenumero
+    mov dx, mensagemSegundoNum
+    mov ah, 9
+    int 21h
     mov bx,num2
     call lenumero
+
     ;Realizar operações (soma(+ 2B), subtração(- 2D), multiplicação(* 2A) e divisão(/ 2F))
+    mov dx, mensagemOperacao
+    mov ah, 9
+    int 21h
+
     ;Lê qual o operador
     mov ah,1
     int 21h
+
     ; Verifica se o ascii passado é a soma
     cmp al, 0x2B
     je soma
+
     ; Verifica se o ascii passado é a subtração
     cmp al, 0x2D
     je subt
+    
     ; Verifica se o ascii passado é a Multiplicação
     cmp al, 0x2A
     je mult
+    
     ; Verifica se o ascii passado é a divisão
     cmp al, 0x2F
     je divi
@@ -52,38 +67,75 @@ segment code
         mov bx, word [num2]
         imul bx
 
+        ;Duas possibilidades de não overflow. DX = 0000 ou DX = FFFF
+
+        cmp dx, 0x0000
+        je semOverflow
+        cmp dx, 0xFFFF
+        je semOverflow
+        jmp overflow ; Caso tenha overflow
+
+        semOverflow:
         mov word [result], ax
         mov word [result+2], dx
         jmp imprimir
         
     divi:
-        mov dx, 0
+        mov dx, 0x0000
         mov ax, word [num1]
+        cmp ax,0
+        jge positivo
+        mov dx, 0xFFFF
+        positivo:
         mov bx, word [num2]
+        cmp bx, 0
+        je erroDivisaoZero
         idiv bx
 
         mov word [result], ax
         jmp imprimir
 
 
-    imprimir: ;Imprimir resultado em decimal
+    ; Imprimir resultado em decimal
+    imprimir:
     MOV 	DI,finalImpressao
-    MOV     AX,word [result]
-    MOV     DX,word [result+2]
-    CALL 	bin2ascii		
+    MOV     DX,word [result]
+    CALL 	bin2ascii	
+
+    MOV     DX, mensagemResultado
+    MOV     AH, 9
+    INT     21h	
 
     MOV 	DX,finalImpressao
     MOV 	AH,9h
-    INT 	21h    
-
+    INT 	21h   
 
     ; Terminar o programa e voltar para o sistema operacional
     mov ah,4ch
-    int 21h
+    int 21h 
 
 
 
     ; FUNÇÕES AUXILIARES
+    overflow:
+        ;Imprimir a mensagem de overflow
+        MOV DX, mensagemOverflow
+        MOV AH, 9
+        INT 21h
+
+        ;finaliza o programa
+        mov ah,4ch
+        int 21h
+    
+    erroDivisaoZero:
+        ;Imprimir a mensagem de erro
+        MOV DX, mensagemDivisaoZero
+        MOV AH, 9
+        INT 21h
+
+        ;finaliza o programa
+        mov ah,4ch
+        int 21h
     
     lenumero:
         PUSHF
@@ -170,12 +222,16 @@ segment code
         PUSH	CX
         PUSH 	DX          
 
-        ;Verificar se o número é maior do que 0x7FFF
-        CMP		DX,0
-        JNE		Uni
-
         ;Verificar se o número é negativo
-        
+        cmp     DX,0
+        jge     numNaoNegativo
+
+        ;Se for negativo, coloca '-' em [DI]
+        MOV     byte [DI],0x2D
+        imul    DX,-1
+
+        numNaoNegativo: 
+
         CMP		DX,10
         JB		Uni
         CMP		DX,100 
@@ -188,61 +244,34 @@ segment code
 
         Uni:	
             ADD		DX,0x0030
-            MOV 	byte [DI],DL
-            RET
+            MOV 	byte [DI+1],DL
+            jmp finishBin2Ascii
         Des:
             MOV 	AX,DX ;0001
             MOV		BL,10
             div		BL
             ADD		AH,0x30
             ADD		AL,0x30
-            MOV 	byte [DI],AL
-            MOV 	byte [DI+1],AH
-            RET
+            MOV 	byte [DI+1],AL
+            MOV 	byte [DI+2],AH
+            jmp finishBin2Ascii
         Cen:		
             MOV 	AX,DX
             MOV		BL,100
             DIV		BL
             ADD		AL,0x30
-            MOV 	byte [DI],AL
+            MOV 	byte [DI+1],AL
             MOV 	AL,AH
             AND		AX,0x00FF
             MOV		BL,10
             DIV		BL
             ADD		AH,0x30
             ADD		AL,0x30
-            MOV 	byte [DI+1],AL		
-            MOV 	byte [DI+2],AH
-            RET
-        Mil:		
-            MOV 	AX,DX
-            MOV     DX,0
-            MOV		BX,1000
-            DIV		BX
-            ADD		AL,0x30
-            MOV 	byte [DI],AL
-            MOV 	AX,DX
-            MOV		BL,100
-            DIV		BL
-            ADD		AL,0x30
-            MOV 	byte [DI+1],AL		
-            MOV 	AL,AH
-            AND     AX,0x00FF
-            MOV		BL,10
-            DIV		BL
-            ADD		AH,0x30
-            ADD		AL,0x30
             MOV 	byte [DI+2],AL		
             MOV 	byte [DI+3],AH
-            RET
-        Dezmil:
+            jmp finishBin2Ascii
+        Mil:		
             MOV 	AX,DX
-            MOV     DX,0
-            MOV		BX,10000
-            DIV		BX
-            ADD		AL,0x30
-            MOV 	byte [DI],AL
-            MOV		AX,DX		
             MOV     DX,0
             MOV		BX,1000
             DIV		BX
@@ -261,25 +290,58 @@ segment code
             ADD		AL,0x30
             MOV 	byte [DI+3],AL		
             MOV 	byte [DI+4],AH
-            RET
+            jmp finishBin2Ascii
+        Dezmil:
+            MOV 	AX,DX
+            MOV     DX,0
+            MOV		BX,10000
+            DIV		BX
+            ADD		AL,0x30
+            MOV 	byte [DI+1],AL
+            MOV		AX,DX		
+            MOV     DX,0
+            MOV		BX,1000
+            DIV		BX
+            ADD		AL,0x30
+            MOV 	byte [DI+2],AL
+            MOV 	AX,DX
+            MOV		BL,100
+            DIV		BL
+            ADD		AL,0x30
+            MOV 	byte [DI+3],AL		
+            MOV 	AL,AH
+            AND     AX,0x00FF
+            MOV		BL,10
+            DIV		BL
+            ADD		AH,0x30
+            ADD		AL,0x30
+            MOV 	byte [DI+4],AL		
+            MOV 	byte [DI+5],AH
+            jmp finishBin2Ascii
 
-
-        ; Upgrade the context
-        POP 	DX
-        POP 	CX
-        POP		BX
-        POP 	AX
-        POPF
-        RET          
+    finishBin2Ascii:
+    ; Upgrade the context
+    POP 	DX
+    POP 	CX
+    POP		BX
+    POP 	AX
+    POPF
+    RET          
                             
                             
 
 segment data
     num1: resb 4
     num2: resb 4
-    result: resb 8 
-    finalImpressao: resb 9 
+    result: resb 4 
+    finalImpressao: resb 5 
                     db 13,10,'$'
+    mensagemOverflow: db 13,10,'Overflow! A operacao ultrapassou o limite de 4 bytes',13,10,'$'
+    mensagemPrimeiroNum: db 13,10,'Insira Primeiro Numero: ',13,10,'$'
+    mensagemSegundoNum: db 13,10,'Insira Segundo Numero: ',13,10,'$'
+    mensagemOperacao: db 13,10,'Insira sinal da operacao: ',13,10,'$'
+    mensagemResultado: db 13,10,'Resultado: ','$'
+    mensagemDivisaoZero: db 13,10,'Erro: Divisao por 0', 13,10,'$'
 
 segment stack stack
     resb 256
